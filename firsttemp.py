b4c7fa52d19ab55e6e96df8a276874bfb2a763bd
unknown <aleksey@sribnyj.com.ua>
2009-02-02 20:34:37 +0200

    Еще коммит Signed-off-by: unknown <aleksey@sribnyj.com.ua>

--- firsttemp.py
+++ firsttemp.py
@@ -1,21 +1,22 @@
 from twill.commands import *
 from twill import *
 from twill import namespaces
-browser = get_browser()
-url = "http://clients.sribnyj.com.ua/test/"
-browser.go(url)
-formclear('1')
-fv("1", "UserLogin", "aleksey")
-fv("1", "UserPassword", "aleksey")
-submit()
-try:
-    find("Bad Karma","m")
-except:
-	try:
-		find("Well Done","m")
-	except:
-		print "Sorry, but some error occured"
-	else:
-		print "Welcome to matrix, Neo"
-else:
-	print "Sorry, wrong login and password"
+def test():
+    browser = get_browser()
+    url = "http://clients.sribnyj.com.ua/test/"
+    browser.go(url)
+    formclear('1')
+    fv("1", "UserLogin", "aleksey")
+    fv("1", "UserPassword", "aleksey")
+    submit()
+    try:
+        find("Bad Karma","m")
+    except:
+        try:
+            find("Well Done","m")
+        except:
+            assert False
+        else:
+            assert True
+    else:
+        assert False
