9d548cb01700e2b5956c8a478e02a01c5d319c6f
unknown <aleksey@sribnyj.com.ua>
2009-02-02 20:04:41 +0200

    Test commit
    Signed-off-by: unknown <aleksey@sribnyj.com.ua>

--- firsttemp.py
+++ firsttemp.py
@@ -1,3 +1,21 @@
 from twill.commands import *
-go("http://www.python.org/")
-showforms()
+from twill import *
+from twill import namespaces
+browser = get_browser()
+url = "http://clients.sribnyj.com.ua/test/"
+browser.go(url)
+formclear('1')
+fv("1", "UserLogin", "aleksey")
+fv("1", "UserPassword", "aleksey")
+submit()
+try:
+    find("Bad Karma","m")
+except:
+	try:
+		find("Well Done","m")
+	except:
+		print "Sorry, but some error occured"
+	else:
+		print "Welcome to matrix, Neo"
+else:
+	print "Sorry, wrong login and password"
